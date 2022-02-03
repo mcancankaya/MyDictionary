db94b5e8d4a144e9667206825d3a73f3517eb522
Can <mccankaya45@hotmail.com>
2022-02-04 01:31:10 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class MyDictionary<Key, Value>
+    {
+        Key[] keys;
+        Value[] values;
+
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+        }
+
+        public void Add(Key key, Value value)
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                if (keys[i].Equals(key))
+                {
+                    Console.WriteLine("Aynı Key Mevcut. Farklı key giriniz. Key : " + key);
+                    return;
+                }
+            }
+
+            Key[] tempKeys;
+            Value[] tempValues;
+
+            tempKeys = keys;
+            tempValues = values;
+
+            keys = new Key[keys.Length + 1];
+            values = new Value[values.Length + 1];
+
+            for (int i = 0; i < keys.Length - 1; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+        public Value[] GetValues()
+        {
+            return values;
+        }
+
+
+        public Value GetByValue(Key key)
+        {
+            int result = -1;
+            for (int i = 0; i < keys.Length; i++)
+            {
+                if (keys[i].Equals(key))
+                {
+                    result = i;
+                }
+
+            }
+            return values[result];
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(5, "Ahmet");
+            dictionary.Add(6, "Can");
+            dictionary.Add(7, "Mert");
+            dictionary.Add(2, "Cansu");
+            dictionary.Add(-1, "Atakan");
+
+
+
+
+            foreach (var item in dictionary.GetValues())
+            {
+
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
